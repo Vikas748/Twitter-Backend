4b61a5dd552546b3504139819fcb83667b7494fa
Vikas Rai <raivikas748@gmail.com>
2026-08-29 20:39:36 +0530

    Added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ const connect=require('./config/database');
 
 const app=express();
 
-const TweetRepository=require('./repository/tweet-repository');
-const Comment=require('./models/comments');
-
 app.listen(3000,async()=>{
     console.log(`Server Started`);
     await connect();
-    console.log(`mongo db connected`)
+    console.log(`mongo db connected`);
 });
