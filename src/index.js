7eab3924695e4b7d6a1a52293ead488cfbaef5ed
Vikas Rai <raivikas748@gmail.com>
2026-08-30 00:37:56 +0530

    Added controllers and routes for create tweet api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import {connect} from './config/database.js';
 
-const app=express();
+import apiRoutes from './routes/index.js';
 
-import TweetService from './services/tweet-service.js'
+const app=express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use('/api',apiRoutes);
 
 app.listen(3000,async()=>{
     console.log(`Server Started`);
     await connect();
     console.log(`mongo db connected`);
-    let ser=new TweetService();
-    await ser.create({content:'create #RAI'});
 });
